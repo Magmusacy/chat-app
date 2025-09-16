2963a27546f77bccdf5b6f4af7fe29e17e3e90ea
Magmusacy <xpfk.098@gmail.com>
2025-09-16 15:35:36 +0200

    Add live displaying of logged in users

---
+++ backend/chatapp/src/main/java/com/magmusacy/chat/chatapp/config/WebSocketEventListener.java
@@ -0,0 +1,51 @@
+package com.magmusacy.chat.chatapp.config;
+
+import com.magmusacy.chat.chatapp.user.User;
+import com.magmusacy.chat.chatapp.user.UserDTO;
+import com.magmusacy.chat.chatapp.user.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.event.EventListener;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.messaging.SessionConnectEvent;
+import org.springframework.web.socket.messaging.SessionDisconnectEvent;
+import org.springframework.web.socket.messaging.SessionSubscribeEvent;
+
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class WebSocketEventListener {
+    private final UserService userService;
+    private final SimpMessagingTemplate messagingTemplate;
+
+    @EventListener
+    public void handleWebSocketConnectListener(SessionConnectEvent event) {
+        System.out.println(event);
+        String email = event.getUser().getName();
+        User user = userService.findByEmail(email);
+        userService.handleUserLogin(user);
+        List<UserDTO> users = userService.findAllUsers();
+        messagingTemplate.convertAndSend("/topic/users", users);
+    }
+
+    @EventListener
+    public void handleWebSocketDisconnectListener(SessionDisconnectEvent event) {
+        System.out.println(event);
+        String email = event.getUser().getName();
+        User user = userService.findByEmail(email);
+        userService.handleUserLogout(user);
+        List<UserDTO> users = userService.findAllUsers();
+        messagingTemplate.convertAndSend("/topic/users", users);
+    }
+
+    @EventListener
+    public void handleWebSocketSubscribeListener(SessionSubscribeEvent event) {
+        String destination = (String) event.getMessage().getHeaders().get("simpDestination");
+        if (destination != null && destination.startsWith("/user/queue/users")) {
+            List<UserDTO> users = userService.findAllUsers();
+            if (event.getUser() == null) return;
+            messagingTemplate.convertAndSendToUser(event.getUser().getName(), "/queue/users", users);
+        }
+    }
+}
